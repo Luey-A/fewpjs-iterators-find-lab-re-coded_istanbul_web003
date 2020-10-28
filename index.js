6121542a95c5c844f1df9590a9bfeecb35c22d24
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-28 13:45:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  { year: "2018", result: "L" },
+  { year: "2017", result: "W" },
+  { year: "2016", result: "N/A" },
+];
+
+const superbowlWin = (arr) => {
+  let win = arr.find((e) => e.result === "W");
+  return win ? win.year : undefined;
+};
+console.log(superbowlWin(record));
